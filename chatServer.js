d0ad9b483c3f0d62abc53c903453f694f9557634
J.D. Zamfirescu <zamfire@gmail.com>
2018-10-09 13:50:09 -0700

    Fixing indentation

--- chatServer.js
+++ chatServer.js
@@ -28,87 +28,80 @@ http.listen(serverPort, function() {
 io.on('connect', function(socket) {
   console.log('a new user connected');
   var questionNum = 0; // keep count of question, used for IF condition.
-  socket.on('loaded', function(){// we wait until the client has loaded and contacted us that it is ready to go.
+  socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
 
-  socket.emit('answer',"Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-  setTimeout(timedQuestion, 2500, socket,"What is your Name?"); // Wait a moment and respond with a question.
+    socket.emit('answer', "Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
+    setTimeout(timedQuestion, 2500, socket, "What is your Name?"); // Wait a moment and respond with a question.
 
-});
-  socket.on('message', (data)=>{ // If we get a new message from the client we process it;
-        console.log(data);
-        questionNum= bot(data,socket,questionNum);	// run the bot function with the new message
-      });
+  });
+  socket.on('message', (data) => { // If we get a new message from the client we process it;
+    console.log(data);
+    questionNum = bot(data, socket, questionNum); // run the bot function with the new message
+  });
   socket.on('disconnect', function() { // This function  gets called when the browser window gets closed
     console.log('user disconnected');
   });
 });
 //--------------------------CHAT BOT FUNCTION-------------------------------//
-function bot(data,socket,questionNum) {
+function bot(data, socket, questionNum) {
   var input = data; // This is generally really terrible from a security point of view ToDo avoid code injection
   var answer;
   var question;
   var waitTime;
 
-/// These are the main statments that make up the conversation.
+  /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-  answer= 'Hello ' + input + ' :-)';// output response
-  waitTime =2000;
-  question = 'How old are you?';			    	// load next question
-  }
-  else if (questionNum == 1) {
-  answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
-  waitTime =2000;
-  question = 'Where do you live?';			    	// load next question
-  }
-  else if (questionNum == 2) {
-  answer= ' Cool! I have never been to ' + input+'.';
-  waitTime =2000;
-  question = 'Whats your favorite Color?';			    	// load next question
-  }
-  else if (questionNum == 3) {
-  answer= 'Ok, ' + input+' it is.';
-  socket.emit('changeBG',input.toLowerCase());
-  waitTime = 2000;
-  question = 'Can you still read the font?';			    	// load next question
-  }
-  else if (questionNum == 4) {
-    if(input.toLowerCase()==='yes'|| input===1){
+    answer = 'Hello ' + input + ' :-)'; // output response
+    waitTime = 2000;
+    question = 'How old are you?'; // load next question
+  } else if (questionNum == 1) {
+    answer = 'Really ' + input + ' Years old? So that means you where born in: ' + (2018 - parseInt(input)); // output response
+    waitTime = 2000;
+    question = 'Where do you live?'; // load next question
+  } else if (questionNum == 2) {
+    answer = ' Cool! I have never been to ' + input + '.';
+    waitTime = 2000;
+    question = 'Whats your favorite Color?'; // load next question
+  } else if (questionNum == 3) {
+    answer = 'Ok, ' + input + ' it is.';
+    socket.emit('changeBG', input.toLowerCase());
+    waitTime = 2000;
+    question = 'Can you still read the font?'; // load next question
+  } else if (questionNum == 4) {
+    if (input.toLowerCase() === 'yes' || input === 1) {
       answer = 'Perfect!';
-      waitTime =2000;
+      waitTime = 2000;
       question = 'Whats your favorite place?';
-    }
-    else if(input.toLowerCase()==='no'|| input===0){
-        socket.emit('changeFont','white'); /// we really should look up the inverse of what we said befor.
-        answer=''
-        question='How about now?';
-        waitTime =0;
-        questionNum--; // Here we go back in the question number this can end up in a loop
-    }else{
-      answer=' I did not understand you. Can you please answer with simply with yes or no.'
-      question='';
+    } else if (input.toLowerCase() === 'no' || input === 0) {
+      socket.emit('changeFont', 'white'); /// we really should look up the inverse of what we said befor.
+      answer = ''
+      question = 'How about now?';
+      waitTime = 0;
+      questionNum--; // Here we go back in the question number this can end up in a loop
+    } else {
+      answer = ' I did not understand you. Can you please answer with simply with yes or no.'
+      question = '';
       questionNum--;
-      waitTime =0;
+      waitTime = 0;
     }
-  // load next question
-  }
-  else{
-    answer= 'I have nothing more to say!';// output response
-    waitTime =0;
+    // load next question
+  } else {
+    answer = 'I have nothing more to say!'; // output response
+    waitTime = 0;
     question = '';
   }
 
 
-/// We take the changed data and distribute it across the required objects.
-  socket.emit('answer',answer);
-  setTimeout(timedQuestion, waitTime,socket,question);
-  return (questionNum+1);
+  /// We take the changed data and distribute it across the required objects.
+  socket.emit('answer', answer);
+  setTimeout(timedQuestion, waitTime, socket, question);
+  return (questionNum + 1);
 }
 
-function timedQuestion(socket,question) {
-  if(question!=''){
-  socket.emit('question',question);
-}
-  else{
+function timedQuestion(socket, question) {
+  if (question != '') {
+    socket.emit('question', question);
+  } else {
     //console.log('No Question send!');
   }
 
